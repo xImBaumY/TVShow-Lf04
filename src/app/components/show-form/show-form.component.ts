0f860febb285c16b0d854b6b1982a894f618f123
xImBaumY <amenschtschikow@gmail.com>
2021-12-10 13:19:42 +0100

    1 file modified, 2 added

---
+++ src/app/components/show-form/show-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-form',
+  templateUrl: './show-form.component.html',
+  styleUrls: ['./show-form.component.css']
+})
+export class ShowFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
