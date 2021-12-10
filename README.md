7d66e72bfd98c35bc4c5bf5bf88f12a952fc8f79
xImBaumY <amenschtschikow@gmail.com>
2021-12-10 09:03:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-w4bhhg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-w4bhhg)
---
+++ src/app/model/show.ts
@@ -0,0 +1,8 @@
+export class Show {
+  public id: number;
+public title: string;
+constructor (id: number, title: string) {
+this.id = id;
+this.title = title;
+}
+}
